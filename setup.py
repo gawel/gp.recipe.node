29a1bb97102c1fa4eb2ee7408e335e9de10b92de
Gael Pasgrimaud <gael@gawel.org>
2011-11-19 13:37:19 +0100

    add manifest so you can install it

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1'
+version = '0.2'
 
 long_description = (
     read('README.txt')
