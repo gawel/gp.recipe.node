8881e608c8386984f8a81465186019381d03d0a1
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-06-02 12:10:14 -0700

    honoring relative paths

--- gp/recipe/node/README.txt
+++ gp/recipe/node/README.txt
@@ -27,6 +27,11 @@ scripts
 node-path
     a list of extra directory to add to ``NODE_PATH``
 
+relaltive-paths
+    will generate paths relative to the root buildout directory.
+    this is also honored if 'relative-paths' is in the main
+    buildout section
+
 
 Example usage
 =============
@@ -51,5 +56,3 @@ Running the buildout gives us::
     Installing test1.
     ...
     Generated script '.../bin/lessc'.
-
-
--- gp/recipe/node/tests.py
+++ gp/recipe/node/tests.py
@@ -3,7 +3,11 @@ import os
 import shutil
 import tempfile
 import subprocess
+import zc.buildout.configparser
 from unittest import TestCase
+from gp.recipe.node import Recipe
+from StringIO import StringIO
+from nose.tools import nottest
 
 BUILDOUT = """
 [buildout]
@@ -26,8 +30,11 @@ scripts =
 
 [node3]
 recipe = gp.recipe.node
-"""
 
+[relativepaths]
+recipe = gp.recipe.node
+relative-paths = true
+"""
 
 class TestNode(TestCase):
 
@@ -74,3 +81,77 @@ class TestNode(TestCase):
         output = self.callFTU('node3')
         self.assertIn(os.path.join(self.wd, 'bin', 'node'), output)
         self.assertIn(os.path.join(self.wd, 'bin', 'npm'), output)
+
+    def test_relative_paths(self):
+        self.callFTU('relativepaths')
+        node_path = os.path.join(self.wd, 'bin', 'node')
+        with open(node_path) as fd:
+            content = fd.read()
+            self.assertIn(
+                "os.environ[\"NODE_PATH\"] = join(base, 'parts",
+                content
+            )
+
+
+class TestNodeClass(TestCase):
+    """ class to unit test the recipe directly """
+
+    def setUp(self):
+        self.buildout = zc.buildout.configparser.parse(
+            StringIO(BUILDOUT),
+            'buildout.cfg')
+        self.name = 'test'
+        self.options = {}
+        self.recipe = Recipe(self.buildout,
+                             self.name,
+                             self.options)
+
+    def test_to_relative(self):
+        """ _to_relative should convert absolute buildout paths to relative """
+        buildout_dir_path = 'foo'
+        self.buildout['buildout']['directory'] = buildout_dir_path
+        test_suffix = os.path.join(
+            "parts", "node", "bin", "node"
+        )
+        test_absolute_path = os.path.join(
+            buildout_dir_path, test_suffix
+        )
+        self.assertEquals(
+            self.recipe._to_relative(test_absolute_path),
+            "join(base, '{0}')".format(test_suffix)
+        )
+
+    def test_get_path(self):
+        """
+        _get_path should choose to change the path to relative
+        or not based on the buildout configuration
+        """
+        buildout_dir_path = 'foo'
+        self.buildout['buildout']['directory'] = buildout_dir_path
+        absolute_path = os.path.join(
+            buildout_dir_path, 'parts', 'node', 'bin', 'node'
+        )
+        self.recipe._use_relative_paths = True
+        self.assertTrue(
+            'join(base' in self.recipe._get_path(absolute_path)
+        )
+        self.recipe._use_relative_paths = False
+        self.assertFalse(
+            'join(base' in self.recipe._get_path(absolute_path)
+        )
+
+    def test_determine_use_relative_paths(self):
+        """ relative paths should be honored """
+
+        self.options['relative-paths'] = 'false'
+        self.assertFalse(self.recipe._determine_use_relative_paths())
+        del(self.options['relative-paths'])
+
+        self.buildout['buildout']['relative-paths'] = 'true'
+        self.assertTrue(self.recipe._determine_use_relative_paths())
+
+        self.buildout['buildout']['relative-paths'] = 'false'
+        self.assertFalse(self.recipe._determine_use_relative_paths())
+
+        self.options['relative-paths'] = 'true'
+        self.assertTrue(self.recipe._determine_use_relative_paths())
